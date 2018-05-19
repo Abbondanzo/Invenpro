8a7076e837b088c8080e14b7d15e82d8439f9ca6
Abbondanzo <peter@abbondanzo.com>
2018-05-19 01:53:34 -0700

    Firebase initialization

--- app/actions/utilActions.ts
+++ app/actions/utilActions.ts
@@ -2,23 +2,22 @@ import { Action } from "redux";
 import { KeylessFirebaseConfig } from "reducers/util";
 
 export const Types = {
-    saveConfig: "SAVE_CONFIG"
+	saveConfig: "SAVE_CONFIG"
 }
 
 /**
  * An action containing a payload of type T.
  */
 export interface IUtilAction<T> extends Action {
-    readonly type: String;
-    readonly payload: T;
+	readonly type: String;
+	readonly payload: T;
 }
 
 export interface IUtilActionWithPromise<T> extends IUtilAction<Promise<T>> { }
 
 export function saveConfig(config: KeylessFirebaseConfig): IUtilAction<KeylessFirebaseConfig> {
-    console.log('test')
-    return {
-        type: Types.saveConfig,
-        payload: config
-    }
+	return {
+		type: Types.saveConfig,
+		payload: config
+	}
 }
--- app/utils/firebaseMiddleware.ts
+++ app/utils/firebaseMiddleware.ts
@@ -1,21 +1,37 @@
-// import * as firebase from 'firebase';
+import * as firebase from 'firebase';
 import { FirebaseConfig } from 'reducers/util';
 import { saveConfig } from 'actions/utilActions';
 // import Store = require("electron-store");
 
 export function initializeFirebase(config: FirebaseConfig) {
-    return (dispatch: Function, getState: Function) => {
+	return (dispatch: Function, getState: Function) => {
+		try {
+			// If we import firebase, check that we use the correct initialization
+			let firebaseInit = firebase
+			if ((firebase as any).default) {
+				firebaseInit = (firebase as any).default
+			}
 
-        try {
-            // TODO: Fix firebase initialization
-            // TODO: Use commented functions below to dispatch successful launches
-            // firebase.initializeApp(config);
-        } catch (error) {
-            console.log(error)
-        }
-        dispatch(saveConfig(config))
-    }
+			// One way or another, we need to initialize the app
+			let init = () => {
+				let firebaseInstance = firebaseInit.initializeApp(config)
+				console.log(firebaseInstance)
+				// Save the instance in a dispatch
+			}
 
+			// Check if we have already initialized a firebase app. If so, delete it
+			if (firebaseInit.apps.length) {
+				firebaseInit.app()
+					.delete()
+					.then(init)
+			} else {
+				init()
+			}
+		} catch (error) {
+			console.log(error)
+		}
+		dispatch(saveConfig(config))
+	}
 }
 
 // function loadFirebase() {
